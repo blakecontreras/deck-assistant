3b657fc9252c853b8a33195507c647f0e777f0e7
Blake Contreras <shardsofsilver@gmail.com>
2016-07-06 09:50:34 -0700

    Improve land name formatting. Remove placeholder numbers from testing.

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -2,11 +2,11 @@ var main = angular.module('deck-assistant.main', [])
 
 .controller('MainController', function($scope, $state, Lands) {
   $scope.mana = [
-    { color: 'White', landType: 'Plains', amount: 24 },
-    { color: 'Blue', landType: 'Island', amount: 0 },
-    { color: 'Black', landType: 'Swamp', amount: 0 },
-    { color: 'Red', landType: 'Mountain', amount: 0 },
-    { color: 'Green', landType: 'Forest', amount: 0 },
+    { color: 'White', landType: 'Plains', amount: 0 },
+    { color: 'Blue', landType: 'Islands', amount: 0 },
+    { color: 'Black', landType: 'Swamps', amount: 0 },
+    { color: 'Red', landType: 'Mountains', amount: 0 },
+    { color: 'Green', landType: 'Forests', amount: 0 },
     { color: 'Colorless', landType: Lands.getColorlessLands($scope.mana), amount: 0 }
   ];
 
